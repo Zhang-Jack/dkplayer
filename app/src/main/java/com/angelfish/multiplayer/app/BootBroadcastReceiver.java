c0d733e0ced1b1f176d4a3edb92ef2c30326d13f
Zhang-Jack <ljack6996@gmail.com>
2019-05-26 19:18:16 +0800

    start a service after boot completed

---
+++ app/src/main/java/com/angelfish/multiplayer/app/BootBroadcastReceiver.java
@@ -0,0 +1,27 @@
+package com.angelfish.multiplayer.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+import com.angelfish.multiplayer.services.PlayAdsService;
+
+public class BootBroadcastReceiver extends BroadcastReceiver {
+    static final String ACTION = "android.intent.action.BOOT_COMPLETED";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(ACTION)) {
+            Intent startIntent = new Intent(context, PlayAdsService.class);  // 要启动的Activity
+            //1.如果自启动APP，参数为需要自动启动的应用包名
+            //Intent intent = getPackageManager().getLaunchIntentForPackage(packageName);
+            //下面这句话必须加上才能开机自动运行app的界面
+            startIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            //2.如果自启动Activity
+            //context.startActivity(intent);
+            //3.如果自启动服务
+            context.startService(startIntent);
+        }
+    }
+}
